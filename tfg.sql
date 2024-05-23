a8adc6c9548bc131d8a81fd72b3244cb7aad0b51
SergioBenitoA <sergy2210@gmail.com>
2024-05-23 21:59:02 +0200

    a

--- tfg.sql
+++ tfg.sql
@@ -1,19 +1,23 @@
+CREATE SEQUENCE usuarios_sequence START 1;
+
 CREATE TABLE USUARIOS (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT default nextval('usuarios_sequence') PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     correo VARCHAR(255) NOT NULL,
     telefono VARCHAR(15),
     contrasena VARCHAR(255) NOT NULL
 );
 
+CREATE SEQUENCE reservas_sequence START 1;
+ 
 CREATE TABLE RESERVAS (
-    id_reserva INT AUTO_INCREMENT PRIMARY KEY,
+    id_reserva INT default nextval('reservas_sequence') PRIMARY KEY,
     codigo_reserva INT NOT NULL,
     dni VARCHAR(10) NOT NULL,
     matricula_vehiculo VARCHAR(10),
     numero_personas INT NOT NULL,
     fecha_entrada DATE NOT NULL,
     fecha_salida DATE NOT NULL,
-    id_usuario INT,
+    id_usuario INT not NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
 );
